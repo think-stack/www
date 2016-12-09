de3898c883133c59b7e2d4aa5cbc66eb9791aab2
Asa Smith <smith.asa.la@gmail.com>
2016-12-09 09:28:35 -0500

    :rocket: initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HardSourcePlugin = require('hard-source-webpack-plugin')
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('babel-preset-latest')
+const pageId = require('spike-page-id')
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md'],
+  reshape: (ctx) => {
+    return htmlStandards({
+      webpack: ctx,
+      locals: { pageId: pageId(ctx), foo: 'bar' }
+    })
+  },
+  postcss: (ctx) => {
+    return cssStandards({ webpack: ctx })
+  },
+  babel: { presets: [jsStandards] },
+  plugins: [
+    new HardSourcePlugin({
+      environmentPaths: { root: __dirname },
+      recordsPath: path.join(__dirname, '_cache/records.json'),
+      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
+    })
+  ]
+}
