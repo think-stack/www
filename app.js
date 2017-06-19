99df9d1e42adf21a9a0ee869fc5c3e33fec1f4f5
Asa Smith <smith.asa.la@gmail.com>
2017-06-19 15:09:23 -0400

    update spike to v2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const path = require('path')
-const HardSourcePlugin = require('hard-source-webpack-plugin')
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
-const jsStandards = require('babel-preset-latest')
+const jsStandards = require('babel-preset-env')
+const dynamicImport = require('babel-plugin-syntax-dynamic-import')
 const pageId = require('spike-page-id')
 
 module.exports = {
@@ -12,22 +12,12 @@ module.exports = {
     css: '*(**/)*.sss'
   },
   ignore: ['**/layout.sgr', '**/.*', '_cache/**', 'readme.md', '**/assets/css/_*'],
-  reshape: (ctx) => {
-    return htmlStandards({
-      webpack: ctx,
-      locals: { pageId: pageId(ctx), foo: 'bar' }
-    })
-  },
-  postcss: (ctx) => {
-    return cssStandards({ webpack: ctx })
-  },
-  babel: { presets: [jsStandards] },
+  reshape: htmlStandards({
+      locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
+    }),
+  postcss: cssStandards(),
+  babel: { presets: [[jsStandards, { modules: false }]], plugins: [dynamicImport] },
   server: {open: false},
   plugins: [
-    new HardSourcePlugin({
-      environmentPaths: { root: __dirname },
-      recordsPath: path.join(__dirname, '_cache/records.json'),
-      cacheDirectory: path.join(__dirname, '_cache/hard_source_cache')
-    })
   ]
 }
