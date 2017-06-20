7f7d4c242e4bd70a9c721a4f259582d028721ddf
Asa Smith <smith.asa.la@gmail.com>
2017-06-20 18:54:59 -0400

    check localStorage for visited value

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,16 @@
 console.log('you can use ES6 here : )')
 
-var serviceUl = document.querySelector('#services ul')
-var services = document.querySelectorAll('#services ul li')
+let visited = localStorage.getItem('visited')
+let modal = document.getElementById('modal')
 
-serviceUl.addEventListener('click', activeListItem, false)
-
-function activeListItem (e) {
-  if (e.target !== e.currentTarget) {
-    for (var i = 0; i < services.length; i++) {
-      services[i].classList.remove('active')
-    }
-    e.target.classList.add('active')
+window.onload = function () {
+  if (visited != 'true') {
+    console.log(modal)
+    localStorage.setItem('visited', true)
+    console.log(localStorage.getItem('visited'))
+  } else {
+    modal.classList.remove('open')
+    modal.classList.add('closed')
+    console.log(modal)
   }
-  e.stopPropagation()
 }
-$(document).ready(function () {
-
-  $('#services li:nth-child(1)').click(function () {
-    $('#service-desc p:nth-child(1)').removeClass('hide')
-    $('#service-desc p:nth-child(2)').addClass('hide')
-    $('#service-desc p:nth-child(3)').addClass('hide')
-    $('#service-desc p:nth-child(4)').addClass('hide')
-  })
-
-  $('#services li:nth-child(2)').click(function () {
-    $('#service-desc p:nth-child(2)').removeClass('hide')
-    $('#service-desc p:nth-child(1)').addClass('hide')
-    $('#service-desc p:nth-child(3)').addClass('hide')
-    $('#service-desc p:nth-child(4)').addClass('hide')
-  })
-
-  $('#services li:nth-child(3)').click(function () {
-    $('#service-desc p:nth-child(3)').removeClass('hide')
-    $('#service-desc p:nth-child(2)').addClass('hide')
-    $('#service-desc p:nth-child(1)').addClass('hide')
-    $('#service-desc p:nth-child(4)').addClass('hide')
-  })
-
-  $('#services li:nth-child(4)').click(function () {
-    $('#service-desc p:nth-child(4)').removeClass('hide')
-    $('#service-desc p:nth-child(2)').addClass('hide')
-    $('#service-desc p:nth-child(3)').addClass('hide')
-    $('#service-desc p:nth-child(1)').addClass('hide')
-  })
-});
