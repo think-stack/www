c069e8d815387ec19edff5f409190827c1a5aa07
Asa Smith <smith.asa.la@gmail.com>
2017-06-21 09:48:18 -0400

    use local storage to only show modal on first visit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,23 +4,25 @@ console.log('you can use ES6 here : )')
 
 let visited = localStorage.getItem('visited')
 let modal = document.getElementById('modal')
+const overlay = document.getElementById('modal-overlay')
 
-/*window.onload = function () {*/
-  //if (visited != 'true') {
-    //console.log(modal)
-    //localStorage.setItem('visited', true)
-    //console.log(localStorage.getItem('visited'))
-  //} else {
-    //modal.classList.remove('open')
-    //modal.classList.add('closed')
-    //console.log(modal)
-  //}
-/*}*/
+window.onload = function () {
+  if (visited != 'true') {
+    console.log(modal)
+    localStorage.setItem('visited', true)
+    console.log(localStorage.getItem('visited'))
+  } else {
+    modal.classList.remove('open')
+    modal.classList.add('closed')
+    overlay.classList.remove('open')
+    overlay.classList.add('closed')
+    console.log(modal)
+  }
+}
 
 // close modal
 
 const modalButton = document.getElementById('button')
-const overlay = document.getElementById('modal-overlay')
 
 modalButton.addEventListener('click', closeModal, false)
 overlay.addEventListener('click', closeModal, false)
