20e80b2aef3d3bbd0bd12c8eff4342ccea320710
Asa Smith <smith.asa.la@gmail.com>
2018-04-12 21:06:54 -0400

    Search (#61)
    * add algolia scripts
    
    * add search functions
    
    * remove link color from global css
    
    * add search icon
    
    * remove input/textarea during focus
    
    * style search
    
    * add page results to search render function
    
    * add algolia logos
    
    * js v1
    
    * adjust search result column width
    
    * add new search icon
    
    * add closing icon for search bar
    
    * finish search

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,207 @@
+const client = algoliasearch('NDZKXKRSA7', 'e4dde62d4e2f8323b8f46d3825bbd8fc')
+const helper = algoliasearchHelper(client, 'test_thinkstack')
+
+helper.on('result', function (content) {
+  renderHits(content)
+})
+
+function renderHits (content) {
+  // assign search results to variable
+  let results = content.hits
+  console.log(results)
+
+  let team = []
+  let blog = []
+  let page = []
+
+  // define container
+  let searchPanel = document.getElementById('search-panel')
+  let resultsContainer = document.createElement('section')
+  resultsContainer.id = 'results-container'
+  searchPanel.appendChild(resultsContainer)
+
+  // sort data into team, blog, and page arrays
+  // todo: add case studies
+  if (results !== undefined) {
+    results.forEach(function (result) {
+      if (result.hasOwnProperty('name')) {
+        team.push(result)
+      } else if (result.hasOwnProperty('title')) {
+        blog.push(result)
+      } else if (result.hasOwnProperty('pageTitle')) {
+        page.push(result)
+      }
+    })
+  }
+
+  if (page.length) {
+    // create div for page search results
+    let pageList = document.createElement('div')
+    pageList.id = 'page'
+
+    let header = document.createElement('h3')
+    header.innerHTML = 'Pages'
+    pageList.appendChild(header)
+
+    let ul = document.createElement('ul')
+    pageList.appendChild(ul)
+
+    resultsContainer.appendChild(pageList)
+
+    page.slice(0, 5).forEach(function (i) {
+      let li = document.createElement('li')
+      let anchor = document.createElement('a')
+      anchor.innerHTML = i.pageTitle
+      anchor.href = i.pageTitle === 'Home' ? '/' : `/${i.pageTitle}`
+      li.appendChild(anchor)
+      ul.appendChild(li)
+    })
+  }
+  if (blog.length) {
+    // create div for blog search results
+    let blogList = document.createElement('div')
+    blogList.id = 'blog'
+
+    let header = document.createElement('h3')
+    header.innerHTML = 'Articles'
+    blogList.appendChild(header)
+
+    let ul = document.createElement('ul')
+    blogList.appendChild(ul)
+
+    resultsContainer.appendChild(blogList)
+
+    // add search results to list
+    blog.slice(0, 5).forEach(function (i) {
+      let li = document.createElement('li')
+      let anchor = document.createElement('a')
+      anchor.innerHTML = i.title
+      anchor.href = i.mediumUrl
+      anchor.target = '_blank'
+      li.appendChild(anchor)
+      ul.appendChild(li)
+    })
+  }
+  if (team.length) {
+    // create div for team results
+    let teamList = document.createElement('div')
+    teamList.id = 'team'
+
+    let header = document.createElement('h3')
+    header.innerHTML = 'Team Members'
+    teamList.appendChild(header)
+
+    let ul = document.createElement('ul')
+    teamList.appendChild(ul)
+
+    resultsContainer.appendChild(teamList)
+
+    // add search results to list
+    team.forEach(function (i) {
+      let li = document.createElement('li')
+      let anchor = document.createElement('a')
+      anchor.innerHTML = i.name
+      anchor.href = '/about'
+      li.appendChild(anchor)
+      ul.appendChild(li)
+    })
+  }
+}
+
+// toggle active class
+function toggleActiveClass (element) {
+  let target = document.getElementById(element)
+
+  if (target.classList.contains('active')) {
+    target.classList.remove('active')
+  } else {
+    target.classList.add('active')
+  }
+}
+
+// add active class
+function addClass (element) {
+  let target = document.getElementById(element)
+
+  target.classList.add('active')
+}
+
+function clearSearch (element) {
+  let input = document.getElementById(element)
+  input.value = ''
+  searchPanel.innerHTML = ''
+}
+
+const searchIcon = document.getElementById('search-icon')
+
+searchIcon.addEventListener('click', function () {
+  let closeX = document.getElementById('close-icon')
+  if (searchIcon.display !== 'none') {
+    searchIcon.display = 'none'
+    closeX.display = 'block'
+  }
+})
+
+// close search panel
+const searchPanel = document.getElementById('search-panel')
+
+// close search icon
+const closeIcon = document.getElementById('close-icon')
+
+closeIcon.addEventListener('click', function () {
+  const body = document.querySelector('body')
+  closeIcon.classList.remove('show')
+  closeIcon.classList.add('hide')
+  searchIcon.classList.remove('hide')
+  searchIcon.classList.add('show')
+  body.style.overflow = ''
+
+  toggleActiveClass('toggle-wrap')
+  toggleActiveClass('search-panel')
+  toggleActiveClass('search-input')
+  toggleActiveClass('algolia-logo')
+  clearSearch('search-input')
+})
+
+searchIcon.addEventListener('click', function () {
+  let navPanel = document.getElementById('nav-panel')
+  let hamburger = document.querySelector('#nav-toggle input[type="checkbox"]')
+  let closeX = document.getElementById('close-icon')
+  let body = document.querySelector('body')
+
+  searchIcon.classList.add('hide')
+  searchIcon.classList.remove('show')
+  closeX.classList.add('show')
+  body.style.overflow = 'hidden'
+
+
+  if (navPanel.classList.contains('active')) {
+    toggleActiveClass('search-input')
+    toggleActiveClass('search-panel')
+    toggleActiveClass('algolia-logo')
+  } else {
+    toggleActiveClass('toggle-wrap')
+    toggleActiveClass('search-panel')
+    toggleActiveClass('search-input')
+    toggleActiveClass('algolia-logo')
+    clearSearch('search-input')
+  }
+})
+
+
+const searchInput = document.getElementById('search-input')
+
+// search listener
+searchInput.addEventListener('keyup', function () {
+  let ul = document.getElementById('search-panel')
+  ul.innerHTML = ''
+  if (this.value === '') {
+    ul.innerHTML = ''
+  } else {
+    helper.setQuery(this.value).search()
+  }
+})
+
 const body = document.getElementsByTagName('body')
 
 function index () {
@@ -91,8 +295,12 @@ const nav = function () {
       navPanel.style.opacity = 1
       navPanel.style.transition = 'opacity .5s ease-out'
       navPanel.style.visibility = 'visible'
+      navPanel.classList.add('active')
       body.style.overflow = 'hidden'
+      toggleActiveClass('toggle-wrap')
     } else {
+      navPanel.classList.remove('active')
+      toggleActiveClass('toggle-wrap')
       navPanel.style.opacity = 0
       navPanel.style.transition = 'opacity .5s ease-in-out'
       body.style.removeProperty('overflow')
@@ -101,7 +309,6 @@ const nav = function () {
       }, 450)
     }
   }, false)
-
 }
 
 nav()
