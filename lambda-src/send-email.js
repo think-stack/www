a2870ddc1c1ba8d9e9ff243fb041186f7cc32c99
Asa Smith <smith.asa.la@gmail.com>
2019-08-09 15:26:18 -0400

    bug fixing

--- lambda-src/send-email.js
+++ lambda-src/send-email.js
@@ -10,12 +10,6 @@ const options = {
 const client = new SparkPost(process.env.SPARKPOST_API_KEY, options);
 import EmailTemplate from '../assets/js/email-template'
 
-const statusCode = 200
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type"
-}
-
 exports.handler = function(event, context, callback) {
   console.log(event)
   // console.log(`this is the event body: JSON.parse(${event.body})`)
@@ -34,7 +28,8 @@ exports.handler = function(event, context, callback) {
   .then(data => {
     callback(null, {
       statusCode: 200,
-      body: 'wow',
+      headers,
+      body: JSON.stringify(data),
     })
   })
   .catch(err => {
