ffb3f632bf8dc9c62a0fb66ee3d9ca7a547e9d7a
Asa Smith <smith.asa.la@gmail.com>
2019-08-09 15:39:26 -0400

    bugfixing

--- lambda-src/send-email.js
+++ lambda-src/send-email.js
@@ -29,7 +29,7 @@ exports.handler = function(event, context, callback) {
     callback(null, {
       statusCode: 200,
       headers,
-      body: JSON.stringify(data),
+      body: data,
     })
   })
   .catch(err => {
